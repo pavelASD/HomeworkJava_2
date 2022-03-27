459bf0ee09c862b901dfb0982ecc39181656a1f0
pavel <acc.for.code.skill@gmail.com>
2022-03-27 21:25:25 +0300

    answer

---
+++ src/main/java/homework1/Course.java
@@ -0,0 +1,28 @@
+package homework1;
+
+public class Course {
+
+    Team team = new Team();
+
+    private int[] courseArray = new int[3];
+
+    public Course(int[] courseArray) {
+        this.courseArray = courseArray;
+    }
+
+    public void dolt(Team team){
+        int sumCourse = 0;
+        for (int i=0; i< courseArray.length; i++){
+            sumCourse = sumCourse+courseArray[i];
+        }
+        if (sumCourse < team.getTotalAmountOfStamina()){
+            team.showInfoIfTeamCouldPassCourse();
+        } else if (sumCourse > team.getTotalAmountOfStamina()){
+            team.showInfoIfTeamCouldNotPassCourse();
+        } else
+            System.out.println("ERROR");
+    }
+}
+
+
+
--- src/main/java/homework1/Main.java
+++ src/main/java/homework1/Main.java
@@ -2,6 +2,10 @@ package homework1;
 
 public class Main {
     public static void main(String[] args) {
-        
+
+    Team firstTeam = new Team("Alpha", new String[]{"James", "Thomas", "Alice", "Katy"}, 1800);
+    Course course = new Course(new int[]{120, 290, 700, 300});
+    course.dolt(firstTeam);
+
     }
 }
